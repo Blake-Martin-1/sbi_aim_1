fa28ed5c8bd6ac4066053f1476f8f51677b55410
Blake Martin <Blake.Martin@childrenscolorado.org>
2026-05-19 12:19:56 -0600

    edited explore retro predictors script to ensure correct full list of predictors is used

--- explore_retro_n_predictors.R
+++ explore_retro_n_predictors.R
@@ -2,15 +2,39 @@
 
 # First divide the rf_df into the train and test data frames by random 80/20 split
 set.seed(seed = 32313)
+
 train_size <- floor(nrow(rf_df) * 0.8)
-sample <- sample.int(n = nrow(rf_df), size = train_size, replace = FALSE)
-train_df <- rf_df[sample, ] #8228 pts
-test_df <- rf_df[-sample, ] # 2058 patients
 
+train_index <- sample.int(
+  n = nrow(rf_df),
+  size = train_size,
+  replace = FALSE
+)
 
-# # Now will train / creat the RF model for abx-unexposed patients
-# Train an initial RF model across all mtry and gini type values
-rf_model_trial <- train(sbi_present ~ ., tuneLength = 10, data = train_df, method = "ranger", na.action = na.omit, importance = 'impurity',
-                        respect.unordered.factors = TRUE,
-                        trControl = trainControl(method = "cv", number = 5, summaryFunction = twoClassSummary, classProbs = TRUE,
-                                                 verboseIter = TRUE))
+train_df <- rf_df[train_index, ]
+test_df  <- rf_df[-train_index, ]
+
+predictors_full_retro <- names(
+  train_df %>%
+    dplyr::select(-case_id, -sbi_present)
+)
+
+rf_train_df <- train_df %>%
+  dplyr::select(sbi_present, dplyr::all_of(predictors_full_retro))
+
+rf_model_trial <- caret::train(
+  sbi_present ~ .,
+  data = rf_train_df,
+  method = "ranger",
+  tuneLength = 10,
+  na.action = na.omit,
+  importance = "impurity",
+  respect.unordered.factors = TRUE,
+  trControl = caret::trainControl(
+    method = "cv",
+    number = 5,
+    summaryFunction = caret::twoClassSummary,
+    classProbs = TRUE,
+    verboseIter = TRUE
+  )
+)
